7a4683b0375ab112c365421dec435de4341758d3
Sourav Potbhare <91159191+souravPotbhare@users.noreply.github.com>
2023-08-26 03:14:53 +0530

    Update and add comments in calculator.js file

--- calculator.js
+++ calculator.js
@@ -4,20 +4,27 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get("/", function(req, res){
-//   res.sendFile(__dirname + "/index.html")
-// });
-//
-// app.post("/", function(req, res){
-//
-// var num1 = Number(req.body.num1);
-// var num2 = Number(req.body.num2);
-//
-// var result = num1 + num2;
-//
-//   res.send("Your Calculation is " + result);
-// });
 
+// Diplaying HTML interface to the user when they open the app
+// display content of index.html file
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html")
+});
+
+// Display Output for their required querry
+app.post("/", function(req, res){
+
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+
+var result = num1 + num2;
+
+  res.send("Your Calculation is " + result);
+});
+
+
+// Another calculator request which
+// Diplaying HTML interface of bmiCalculator.html file
 app.get("/bmicalculator", function(req, res){
   res.sendFile(__dirname + "/bmiCalculator.html")
 })
@@ -29,9 +36,12 @@ var weight = parseFloat(req.body.weight);
 
 var bmi = ( weight / (height*height));
 
+// Display Output for their required querry
   res.send("Your BMI is " + bmi );
 })
 
+// when using this app locally, it will start on port 3000 using Node.js
+// once you req node, you can access it on, localhost:3000
 app.listen(2000, function(){
   console.log("Hello welcome to server 2000")
 });
