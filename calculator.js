8bf4a54b3799cb7139819850106ab764ee85ab9c
Sourav Potbhare <91159191+souravPotbhare@users.noreply.github.com>
2023-08-28 22:37:44 +0530

    Update calculator.js with other operations

--- calculator.js
+++ calculator.js
@@ -7,19 +7,36 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Diplaying HTML interface to the user when they open the app
 // display content of index.html file
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function (req, res) {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+  const operation = req.body.operation;
+
+  let result;
+
+  switch (operation) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "divide":
+      result = num1 / num2;
+      break;
+    default:
+      result = "Invalid operation";
+  }
+  
 // Display Output for their required querry
-app.post("/", function(req, res){
-
-var num1 = Number(req.body.num1);
-var num2 = Number(req.body.num2);
-
-var result = num1 + num2;
-
-  res.send("Your Calculation is " + result);
+  res.send(`Your Calculation is ${result}`);
 });
 
 
@@ -39,6 +56,7 @@ var bmi = ( weight / (height*height));
 // Display Output for their required querry
   res.send("Your BMI is " + bmi );
 })
+
 
 // when using this app locally, it will start on port 3000 using Node.js
 // once you req node, you can access it on, localhost:3000
